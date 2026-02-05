4f49497fdd74e48159eb45b8ca602c1b139c27db
Racheli1979 <rachelidavidovizh@gmail.com>
2026-02-05 15:01:40 +0200

    🗄️ Adding backend logic to fetch all database tables

--- backend/TableManagementApi/Program.cs
+++ backend/TableManagementApi/Program.cs
@@ -1,9 +1,23 @@
+using DotNetEnv;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TableManagementBl.Services;
+using TableManagementDal.Repositories;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Env.Load();
+
+string connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+
+// 🔹 Register DAL and BL
+builder.Services.AddScoped<TablesRepository>(_ => new TablesRepository(connectionString));
+builder.Services.AddScoped<TablesService>();
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -16,7 +30,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
---
+++ backend/TableManagementDal/Models/TableMetadataDo.cs
@@ -0,0 +1,9 @@
+namespace TableManagementDal.Models
+{
+    public class TableMetadataDo
+    {
+        public string TableName { get; set; }
+        public string SchemaName { get; set; }
+        public string ObjectType { get; set; }
+    }
+}
